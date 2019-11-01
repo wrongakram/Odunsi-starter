523c4d1ce874cdb2fd6dd5aa53534409a78888a3
Akram Khalid <akramkhalid12@gmail.com>
2019-11-01 11:25:15 -0500

    init commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,8 @@
-import { Link } from "gatsby"
 import React from "react"
 
 const Header = () => (
   <header>
-    <div className="container">
-      <div className="inner-header">
-        <div className="logo">
-          <Link to="/">ODUNSI</Link>
-        </div>
-        <div className="navigation">
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/work">Work</Link>
-            <Link to="/contact">Contact</Link>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <p>header</p>
   </header>
 )
 
